9f6781ec042a805830e0aa88b9f67cab048c1b53
Pranav Sanghi <112352133+psanghi04@users.noreply.github.com>
2024-01-20 18:42:25 -0500

    Update lexical_analysis.py

--- lexical/lexical_analysis.py
+++ lexical/lexical_analysis.py
@@ -1,16 +1,8 @@
 import vaderSentiment as vader
-import speech_recognition as sr
+import speech_to_text as stt
 
 analyzer = vader.SentimentIntensityAnalyzer()
 
-def calc_score(paragraph):
-    sentences_raw = paragraph.split("|")
-    sentences = []
-    return_vec = []
-    for i in range(len(sentences_raw)):
-        if sentences_raw[i].split(":")[0] == "Interviewee":
-            sentences.append(sentences_raw[i].split(":")[1])
-    for response in sentences:
-        vs = analyzer.polarity_scores(response)
-        return_vec.append(vs)
-    return return_vec
+def calc_score():
+    response = stt.transcribe()
+    return analyzer.polarity_scores(response)
